a6876e7efd16debe3fd1b1471d42854039de9ed9
MrBradabie <55830472+MrBradabie@users.noreply.github.com>
2019-11-07 19:32:02 +0700

    Add files via upload

---
+++ IF1-10118006-Latihan49-BiayaEmasKawin/src/if1/pkg10118006/latihan49/biayaemaskawin/Emas.java
@@ -0,0 +1,36 @@
+
+package if1.pkg10118006.latihan49.biayaemaskawin;
+
+/*
+ * @author 
+ * NAMA  : Alfarizi Rizki Pane
+ * KELAS : IF 1
+ * NIM   : 10118006
+ */
+public class Emas {
+    
+   public int hargaGram;
+   public double beratEmas;
+
+    public int getHargaGram() {
+        return hargaGram;
+    }
+
+    public void setHargaGram(int hargaGram) {
+        this.hargaGram = hargaGram;
+    }
+
+    public double getBeratEmas() {
+        return beratEmas;
+    }
+
+    public void setBeratEmas(double beratEmas) {
+        this.beratEmas = beratEmas;
+    }
+   
+    public double perhitungan(int hargaGram, double beratEmas) {
+       
+       return hargaGram * beratEmas ;
+    }
+    
+}
---
+++ IF1-10118006-Latihan49-BiayaEmasKawin/src/if1/pkg10118006/latihan49/biayaemaskawin/IF110118006Latihan49BiayaEmasKawin.java
@@ -0,0 +1,40 @@
+
+package if1.pkg10118006.latihan49.biayaemaskawin;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
+/*
+ * @author 
+ * NAMA  : Alfarizi Rizki Pane
+ * KELAS : IF 1
+ * NIM   : 10118006
+ * Deskripsi Program : Program ini untuk membuat perhitungan untuk mas kawin.
+ */
+public class IF110118006Latihan49BiayaEmasKawin {
+
+    public static void main(String[] args) {
+        
+        DecimalFormat mataUangIndonesia = (DecimalFormat) 
+                      DecimalFormat.getCurrencyInstance();
+        DecimalFormatSymbols formatRP = new DecimalFormatSymbols();
+        
+        formatRP.setCurrencySymbol("Rp.");
+        formatRP.setMonetaryDecimalSeparator(',');
+        formatRP.setGroupingSeparator('.');
+        
+        mataUangIndonesia.setDecimalFormatSymbols(formatRP);
+        
+        
+        Emas objEmas = new Emas();
+        objEmas.setHargaGram(570000);
+        objEmas.setBeratEmas(15.7);
+        System.out.println("====Aplikasi Hitung Harga Emas====");
+        System.out.printf("Total Bayar Emas : %s %n ", 
+                           mataUangIndonesia.format(
+                           objEmas.perhitungan(objEmas.getHargaGram(), 
+                           objEmas.getBeratEmas())));
+
+    }
+    
+}
